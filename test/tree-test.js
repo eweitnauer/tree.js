497b14880e4c5e190c0e0a7a64d95a1a4aa8ad16
Erik Weitnauer <eweitnauer@gmail.com>
2021-05-26 14:07:08 -0400

    cleaning things up - removing istanbul and uglifyjs - using package.json scripts instead of the makefile

--- test/tree-test.js
+++ test/tree-test.js
@@ -1,7 +1,7 @@
 /// Copyright by Erik Weitnauer, 2013.
 
 /// testing with nodeunit
-var Tree   = require('../tree.js').Tree;
+var Tree   = require('../dist/tree.js').Tree;
 
 exports['parse'] = function(test) {
   var t0 = Tree.parse('');
